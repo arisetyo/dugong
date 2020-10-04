c5891a5f3c8ea88d27c1e648213412575562e818
Arie M. Prasetyo <ari.setyo@gmail.com>
2020-10-04 22:34:01 +0700

    added fetch example placeholder

--- curator/src/modules/Logs.js
+++ curator/src/modules/Logs.js
@@ -4,14 +4,37 @@
  * @copyright 2020
  */
 
-import React from 'react';
+import React, {useState} from 'react';
 import styles from './Logs.css';
 
 // PLACEHOLDER
-const Logs = () => (
-  <div className={styles.Logs}>
-    Placeholder Logs 
-  </div>
-);
+const Logs = () => {
+  const [result, setResult] = useState();
+
+  const resultHandler = data => {
+    if (result) return;
+    setResult(data);
+  }
+
+  /**
+	 * retrieve the logs from dispenser
+	 */
+  fetch('http://localhost:3000/api/v1/logs')
+    .then(response => response.json())
+    .then(resultHandler);
+
+  return (
+    <div className={styles.Logs}>
+      Placeholder Logs
+      {
+        result && result.map(log => (
+          <p key={log._id}>
+            {`user ${log.userId} was doing '${log.activity}' on ${log.url}`}
+          </p>
+        ))
+      }
+    </div>
+  );
+};
 
 export default Logs;
